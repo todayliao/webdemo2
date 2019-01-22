31de53ae5ca2d9d1e8b03d261310c452fb323ecc
todayliao <531158251@qq.com>
2019-01-22 16:46:08 +0800

    添加功能

--- src/main/java/org/smart/chapter/service/CustomerService.java
+++ src/main/java/org/smart/chapter/service/CustomerService.java
@@ -1,7 +1,10 @@
 package org.smart.chapter.service;
 
+import org.slf4j.LoggerFactory;
+import org.smart.chapter.helper.DatabaseHelper;
 import org.smart.chapter.model.Customer;
 
+import java.sql.Connection;
 import java.util.List;
 import java.util.Map;
 
@@ -12,23 +15,31 @@ import java.util.Map;
  * @date 2019/1/17
  **/
 public class CustomerService {
+    private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(CustomerService.class);
+
+
     public List<Customer> getCustomerList() {
-        return null;
+
+        Connection conn = DatabaseHelper.getConnection();
+        String sql = "select * from customer";
+        return DatabaseHelper.queryEntityList(Customer.class, sql);
+
     }
 
     public Customer getCustomer(long id) {
-        return null;
+        String sql = "SELECT * FROM customer WHERE id = ?";
+        return DatabaseHelper.queryEntity(Customer.class, sql, id);
     }
 
-    public boolean createCustomer(Map<String, Object> filedMap) {
-        return false;
+    public boolean createCustomer(Map<String, Object> fieldMap) {
+        return DatabaseHelper.insertEntity(Customer.class, fieldMap);
     }
 
-    public boolean updateCustomer(long id, Map<String, Object> fileMap) {
-        return false;
+    public boolean updateCustomer(long id, Map<String, Object> fieldMap) {
+        return DatabaseHelper.updateEntity(Customer.class, id, fieldMap);
     }
 
     public boolean deleteCustomer(long id) {
-        return false;
+        return DatabaseHelper.deleteEntity(Customer.class, id);
     }
 }
